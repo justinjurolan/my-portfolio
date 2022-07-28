bf346977cf4be8ac05d06912d8a6385277591c1b
Justin Jurolan <justinjurolan@gmail.com>
2022-07-28 21:14:51 +0800

    fix home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <Link to="/">
+        <Link to="/my-portfolio">
           <img
             src={logo}
             alt="logo"
